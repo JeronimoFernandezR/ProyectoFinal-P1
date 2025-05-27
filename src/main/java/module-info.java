fcd77c3c776e08c543d9e5ff33ce55d4b4ad491f
JeronimoFernandezR <jeronimo.fernandezr@uqvirtual.edu.co>
2025-05-26 23:08:59 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.uniquindio.edu.co.poo.proyectofinalp1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.uniquindio.edu.co.poo.proyectofinalp1 to javafx.fxml;
-    exports org.uniquindio.edu.co.poo.proyectofinalp1;
-}
